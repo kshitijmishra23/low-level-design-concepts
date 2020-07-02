8bf12b67383245e7f7e9feffc390ab237840dd98
amrojsandhu <amrojpals@gmail.com>
2020-07-02 11:02:44 +0530

    Scaler-200701: SRP on the Employee class

--- src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
@@ -1,10 +1,5 @@
 package oops.SOLID.singleResponsibilityPrinciple.before;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /*
 Models an employee form a business perspective
  */
@@ -14,7 +9,7 @@ public abstract class Employee {
     private int monthlyIncome;
     private int nbHoursPerWeek;
 
-    public Employee(String fullName, int monthlyIncome){
+    public Employee(String fullName, int monthlyIncome) {
         setMonthlyIncome(monthlyIncome);
 
         String[] names = fullName.split(" ");
@@ -28,19 +23,12 @@ public abstract class Employee {
                 "@globomanticshr.com";
     }
 
-    @Override
-    public String toString() {
-        return this.firstName + " " +
-                this.lastName + " - " +
-                this.monthlyIncome;
-    }
-
     public int getMonthlyIncome() {
         return monthlyIncome;
     }
 
     public void setMonthlyIncome(int monthlyIncome) {
-        if(monthlyIncome < 0){
+        if (monthlyIncome < 0) {
             throw new IllegalArgumentException("Income must be positive");
         }
 
@@ -52,42 +40,21 @@ public abstract class Employee {
     }
 
     public void setNbHoursPerWeek(int nbHoursPerWeek) {
-        if(nbHoursPerWeek <= 0){
+        if (nbHoursPerWeek <= 0) {
             throw new IllegalArgumentException("Income must be positive");
         }
 
         this.nbHoursPerWeek = nbHoursPerWeek;
     }
 
-    public String getFullName(){
+    public String getFullName() {
         return this.firstName + " " + this.lastName;
     }
 
-    public static void save(Employee employee){
-        try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("### EMPLOYEE RECORD ####");
-            sb.append(System.lineSeparator());
-            sb.append("NAME: ");
-            sb.append(employee.firstName + " " + employee.lastName);
-            sb.append(System.lineSeparator());
-            sb.append("POSITION: ");
-            sb.append(employee.getClass().getTypeName());
-            sb.append(System.lineSeparator());
-            sb.append("EMAIL: ");
-            sb.append(employee.getEmail());
-            sb.append(System.lineSeparator());
-            sb.append("MONTHLY WAGE: ");
-            sb.append(employee.monthlyIncome);
-            sb.append(System.lineSeparator());
-
-            Path path = Paths.get(employee.getFullName()
-                    .replace(" ","_") + ".rec");
-            Files.write(path, sb.toString().getBytes());
-
-            System.out.println("Saved employee " + employee.toString());
-        } catch (IOException e){
-            System.out.println("ERROR: Could not save employee. " + e);
-        }
+    @Override
+    public String toString() {
+        return this.firstName + " " +
+                this.lastName + " - " +
+                this.monthlyIncome;
     }
 }
