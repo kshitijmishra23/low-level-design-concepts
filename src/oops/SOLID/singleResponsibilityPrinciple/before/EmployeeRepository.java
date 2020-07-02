ad1359f90f36d5773ee69195da6292922b89b1da
Raviteja Reddy <tejareddy1004@gmail.com>
2020-07-03 00:24:37 +0530

    Moved out serializability as a functionality

--- src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
@@ -23,25 +23,9 @@ public class EmployeeRepository {
 
     public static void save(Employee employee){
         try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("### EMPLOYEE RECORD ####");
-            sb.append(System.lineSeparator());
-            sb.append("NAME: ");
-            sb.append(employee.getFullName());
-            sb.append(System.lineSeparator());
-            sb.append("POSITION: ");
-            sb.append(employee.getClass().getTypeName());
-            sb.append(System.lineSeparator());
-            sb.append("EMAIL: ");
-            sb.append(employee.getEmail());
-            sb.append(System.lineSeparator());
-            sb.append("MONTHLY WAGE: ");
-            sb.append(employee.getMonthlyIncome());
-            sb.append(System.lineSeparator());
-
             Path path = Paths.get(employee.getFullName()
                     .replace(" ","_") + ".rec");
-            Files.write(path, sb.toString().getBytes());
+            Files.write(path, EmployeeUtil.serializeEmployee(employee).getBytes());
 
             System.out.println("Saved employee " + employee.toString());
         } catch (IOException e){
