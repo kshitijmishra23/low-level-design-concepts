363848837b5e5e40e0eaa04613caa948c129815b
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-02 21:25:10 +0530

    moving employee related classes to employee package

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/after/employees/FullTimeEmployee.java
@@ -0,0 +1,8 @@
+package oops.SOLID.singleResponsibilityPrinciple.after.employees;
+
+public class FullTimeEmployee extends Employee {
+    public FullTimeEmployee(String fullName, int monthlyIncome) {
+        super(fullName, monthlyIncome);
+        this.setNbHoursPerWeek(40);
+    }
+}
