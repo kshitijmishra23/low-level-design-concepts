5cd672e5d7fed93fae415ec9cdc12a2937f6e0e8
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-02 21:40:08 +0530

    move save method to EmployeeRepository class to maintin SRP in Employee class

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/after/repository/EmployeeRepository.java
@@ -0,0 +1,55 @@
+package oops.SOLID.singleResponsibilityPrinciple.after.repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import oops.SOLID.singleResponsibilityPrinciple.after.employees.Employee;
+import oops.SOLID.singleResponsibilityPrinciple.after.employees.FullTimeEmployee;
+import oops.SOLID.singleResponsibilityPrinciple.after.employees.PartTimeEmployee;
+
+public class EmployeeRepository {
+
+    public List<Employee> findAll(){
+
+        // Employees are kept in memory for simplicity
+        Employee anna = new FullTimeEmployee("Anna Smith", 2000);
+        Employee billy = new FullTimeEmployee("Billy Leech", 920);
+
+        Employee steve = new PartTimeEmployee("Steve Jones", 800);
+        Employee magda = new PartTimeEmployee("Magda Iovan", 920);
+
+        return Arrays.asList(anna, billy, steve, magda);
+    }
+    
+    public static void save(Employee employee){
+        try {
+            StringBuilder sb = new StringBuilder();
+            sb.append("### EMPLOYEE RECORD ####");
+            sb.append(System.lineSeparator());
+            sb.append("NAME: ");
+            sb.append(employee.getFullName());
+            sb.append(System.lineSeparator());
+            sb.append("POSITION: ");
+            sb.append(employee.getClass().getTypeName());
+            sb.append(System.lineSeparator());
+            sb.append("EMAIL: ");
+            sb.append(employee.getEmail());
+            sb.append(System.lineSeparator());
+            sb.append("MONTHLY WAGE: ");
+            sb.append(employee.getMonthlyIncome());
+            sb.append(System.lineSeparator());
+
+            Path path = Paths.get(employee.getFullName()
+                    .replace(" ","_") + ".rec");
+            Files.write(path, sb.toString().getBytes());
+
+            System.out.println("Saved employee " + employee.toString());
+        } catch (IOException e){
+            System.out.println("ERROR: Could not save employee. " + e);
+        }
+    }
+}
