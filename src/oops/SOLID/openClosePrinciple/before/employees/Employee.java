9d7b19e20af2c75241042f6bec5622bd4040cc82
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-03 21:04:43 +0530

    removing unused imports from the Employee class

--- src/oops/SOLID/openClosePrinciple/before/employees/Employee.java
+++ src/oops/SOLID/openClosePrinciple/before/employees/Employee.java
@@ -1,10 +1,5 @@
 package oops.SOLID.openClosePrinciple.before.employees;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /*
 Models an employee form a business perspective
  */
