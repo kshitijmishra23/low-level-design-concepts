a63b6026aa9b8fb124de4b1b604293d124d25727
Rajat Goyal <rajatgoyal715@gmail.com>
2020-07-04 20:44:17 +0530

    Add tax calculator factory and separate calculators

--- src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
+++ src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
@@ -6,7 +6,8 @@ import java.util.Locale;
 
 import oops.SOLID.openClosePrinciple.before.employees.Employee;
 import oops.SOLID.openClosePrinciple.before.persistence.EmployeeRepository;
-import oops.SOLID.openClosePrinciple.before.taxes.TaxCalculator;
+import oops.SOLID.openClosePrinciple.before.taxes.TaxCalculatorFactory;
+import oops.SOLID.openClosePrinciple.before.taxes.Taxable;
 
 
 public class CalculateTaxesClient {
@@ -20,10 +21,12 @@ public class CalculateTaxesClient {
         // Calculate taxes
         Locale locale = new Locale("en", "US");
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
-        TaxCalculator taxCalculator = new TaxCalculator();
 
         double totalTaxes = 0;
         for (Employee employee: employees){
+
+            // get tax calculator for particular employee type
+            Taxable taxCalculator = TaxCalculatorFactory.getTaxCalculator(employee.getEmployeeType());
 
             // compute individual tax
             double tax = taxCalculator.calculate(employee);
@@ -31,5 +34,6 @@ public class CalculateTaxesClient {
             // add to company total taxes
             totalTaxes += taxCalculator.calculate(employee);
         }
+        System.out.println(totalTaxes);
     }
 }
