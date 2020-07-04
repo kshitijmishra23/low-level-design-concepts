1f0372eadf479eab33d33ef5d7878420bba33b8f
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-04 20:03:54 +0530

    Class for subway sandwich orders

---
+++ src/oops/designPatterns/builder/SubWaySandwichOrderV1.java
@@ -0,0 +1,35 @@
+package oops.designPatterns.builder;
+
+public class SubWaySandwichOrderV1 {
+	private String bread;
+    private String condiments;
+    private String veggies;
+    private String fillings;
+    
+	public String getBread() {
+		return bread;
+	}
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+	public String getCondiments() {
+		return condiments;
+	}
+	public void setCondiments(String condiments) {
+		this.condiments = condiments;
+	}
+	public String getVeggies() {
+		return veggies;
+	}
+	public void setVeggies(String veggies) {
+		this.veggies = veggies;
+	}
+	public String getFillings() {
+		return fillings;
+	}
+	public void setFillings(String filling) {
+		this.fillings = filling;
+	}
+    
+
+}
