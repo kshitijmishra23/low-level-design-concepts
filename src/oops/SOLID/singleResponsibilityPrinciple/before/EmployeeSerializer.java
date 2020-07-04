db52c922c90fa8dead8d8fa8e3e6954cf1562f48
nayak-alka <alkanayak564@gmail.com>
2020-07-04 14:24:37 +0530

    Creating separate class EmployeeSerializer
    reason - if it's decided later to change the format of the Employee data String value.

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeSerializer.java
@@ -0,0 +1,27 @@
+package oops.SOLID.singleResponsibilityPrinciple.before;
+
+public class EmployeeSerializer {
+
+	public String serializeEmp(Employee employee) {
+		return serializeColonSpaceFormat(employee);
+	}
+
+	private String serializeColonSpaceFormat(Employee employee) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("### EMPLOYEE RECORD ####");
+		sb.append(System.lineSeparator());
+		sb.append("NAME: ");
+		sb.append(employee.getFirstName() + " " + employee.getLastName());
+		sb.append(System.lineSeparator());
+		sb.append("POSITION: ");
+		sb.append(employee.getClass().getTypeName());
+		sb.append(System.lineSeparator());
+		sb.append("EMAIL: ");
+		sb.append(employee.getEmail());
+		sb.append(System.lineSeparator());
+		sb.append("MONTHLY WAGE: ");
+		sb.append(employee.getMonthlyIncome());
+		sb.append(System.lineSeparator());
+		return sb.toString();
+	}
+}
