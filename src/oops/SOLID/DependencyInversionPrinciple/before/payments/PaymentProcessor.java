f3e19d08ef11a07607aef12be8d39fb4877ce13d
Amey.Kamat <amey.kamat@gds.ey.com>
2020-07-05 11:39:36 +0530

    Refactoring PaymentProcessor to make it more readable

--- src/oops/SOLID/DependencyInversionPrinciple/before/payments/PaymentProcessor.java
+++ src/oops/SOLID/DependencyInversionPrinciple/before/payments/PaymentProcessor.java
@@ -1,30 +1,19 @@
 package oops.SOLID.DependencyInversionPrinciple.before.payments;
 
-import java.util.List;
-
 import oops.SOLID.DependencyInversionPrinciple.before.employees.Employee;
-import oops.SOLID.DependencyInversionPrinciple.before.notification.EmailSender;
-import oops.SOLID.DependencyInversionPrinciple.before.repositories.EmployeeFileRepository;
-import oops.SOLID.DependencyInversionPrinciple.before.serielizer.EmployeeFileSerializer;
+import oops.SOLID.DependencyInversionPrinciple.before.repositories.EmployeeRepository;
 
 public class PaymentProcessor {
 
+    private final EmployeeRepository employeeRepository;
+
+    public PaymentProcessor(final EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
 
     public int sendPayments(){
-    	EmployeeFileSerializer serializer =
-                new EmployeeFileSerializer();
-    	
-    	EmployeeFileRepository employeeRepository = 
-    			new EmployeeFileRepository(serializer);
-    	
-        List<Employee> employees = employeeRepository.findAll();
-        int totalPayments = 0;
-
-        for(Employee employee : employees){
-            totalPayments += employee.getMonthlyIncome();
-            EmailSender.notify(employee);
-        }
-
-        return totalPayments;
+    	return employeeRepository.findAll().stream()
+            .map(Employee::getMonthlyIncome)
+            .reduce(0, Integer::sum);
     }
 }
