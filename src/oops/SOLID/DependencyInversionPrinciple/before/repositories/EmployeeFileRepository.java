76f269c07691d4630b2ff138251bda811d49448e
Amey.Kamat <amey.kamat@gds.ey.com>
2020-07-05 11:31:24 +0530

    Make EmployeeFileRepository conform to interface

--- src/oops/SOLID/DependencyInversionPrinciple/before/repositories/EmployeeFileRepository.java
+++ src/oops/SOLID/DependencyInversionPrinciple/before/repositories/EmployeeFileRepository.java
@@ -16,52 +16,38 @@ import oops.SOLID.DependencyInversionPrinciple.before.employees.Intern;
 import oops.SOLID.DependencyInversionPrinciple.before.employees.PartTimeEmployee;
 import oops.SOLID.DependencyInversionPrinciple.before.serielizer.EmployeeFileSerializer;
 
+import static java.util.Arrays.asList;
+
 /*
 Helper method to perform CRUD operations on employees. In a production
 application we could use the database for persistence. In this demo,
 we are storing employees in the file system.
  */
 
-public class EmployeeFileRepository {
-    private EmployeeFileSerializer serializer;
+public class EmployeeFileRepository implements EmployeeRepository {
+
+    private final EmployeeFileSerializer serializer;
 
     public EmployeeFileRepository(EmployeeFileSerializer serializer) {
         this.serializer = serializer;
     }
 
     public List<Employee> findAll() {
-     // Employees are kept in memory for simplicity
-     		Employee anna = new FullTimeEmployee("Anna Smith", 2000);
-     		Employee billy = new FullTimeEmployee("Billy Leech", 920);
+        final Employee anna = new FullTimeEmployee("Anna Smith", 2000);
+        final Employee billy = new FullTimeEmployee("Billy Leech", 920);
 
-     		Employee steve = new PartTimeEmployee("Steve Jones", 800);
-     		Employee magda = new PartTimeEmployee("Magda Iovan", 920);
+        final Employee steve = new PartTimeEmployee("Steve Jones", 800);
+        final Employee magda = new PartTimeEmployee("Magda Iovan", 920);
 
-     		return Arrays.asList(anna, billy, steve, magda);
+        return asList(anna, billy, steve, magda);
     }
 
-    public void save(Employee employee) throws IOException {
-        String serializedString = this.serializer.serialize(employee);
-
-        Path path = Paths.get(employee.getFullName()
-                .replace(" ", "_") + ".rec");
-        Files.write(path, serializedString.getBytes());
+    public void save(final Employee employee) throws IOException {
+        final Path path = Paths.get( getFileName( employee.getFullName()));
+        Files.write(path, serializer.serialize(employee).getBytes());
     }
 
-    private Employee createEmployeeFromCsvRecord(String line) {
-        String[] employeeRecord = line.split(",");
-        String name = employeeRecord[0];
-        int income = Integer.parseInt(employeeRecord[1]);
-        int nbHours = Integer.parseInt(employeeRecord[2]);
-
-        Employee employee;
-        if(nbHours == 40){
-            employee = new FullTimeEmployee(name,income);
-        } else if (nbHours == 20){
-            employee = new PartTimeEmployee(name, income);
-        } else{
-            employee = new Intern(name, income, nbHours);
-        }
-        return employee;
+    private String getFileName(final String employeeName){
+        return employeeName.replace(" ", "_") + ".rec";
     }
 }
