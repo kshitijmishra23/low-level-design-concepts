0ef7135cf6a1c8309fa8277255d3fbb4d765db89
Amey.Kamat <amey.kamat@gds.ey.com>
2020-07-05 12:47:43 +0530

    Refactoring for singleton pattern

--- src/oops/designPatterns/singleton/Client.java
+++ src/oops/designPatterns/singleton/Client.java
@@ -1,77 +1,50 @@
 package oops.designPatterns.singleton;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.function.Supplier;
 
 public class Client {
 	
 	public void createConnectionUsingNormalConnector() {
+
 		System.out.println("Normal Connection");
-		long timeB = 0;
-		long timeA = 0;
-		
-		timeB = System.currentTimeMillis();
-		SQLConnectionNormal sqlconnection =  new SQLConnectionNormal();
-		Connection connection = sqlconnection.getConnection();
-		timeA = System.currentTimeMillis();
-		System.out.println(timeA-timeB);
+
+		final SqlExecutor executor = timeExecutorCreation(() -> new SqlExecutor(new SQLConnectionNormal()));
 		
 		try {
-		 Statement statement = connection.createStatement();
-	        String selectSql ="select * from users where id = 169997";
-	        ResultSet resultSet = statement.executeQuery(selectSql);
+	        final ResultSet resultSet = executor.execute("select * from users where id = 169997");
 	        while(resultSet.next()) {
-	        System.out.println("id: "+ resultSet.getInt("id"));
-	        System.out.println("name: "+ resultSet.getString("name"));
-	        System.out.println("email: "+ resultSet.getString("email"));
-	        System.out.println("university: "+ resultSet.getString("university"));
+	        	System.out.println("id: "+ resultSet.getInt("id"));
+	        	System.out.println("name: "+ resultSet.getString("name"));
+	        	System.out.println("email: "+ resultSet.getString("email"));
+	        	System.out.println("university: "+ resultSet.getString("university"));
 	        }
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
-		
-		timeB = System.currentTimeMillis();
-		SQLConnectionNormal sqlconnection2 = new SQLConnectionNormal();
-		Connection connection2 = sqlconnection.getConnection();
-		timeA = System.currentTimeMillis();
-		System.out.println(timeA-timeB);	
-		
+
+		timeExecutorCreation(() -> new SqlExecutor(new SQLConnectionNormal()));
 		System.out.println("------------------------------\n");
 	}
 	
 	public void createConnectionUsingSingletonConnector() {
 		System.out.println("Singleton Connection");
-		long timeB = 0;
-		long timeA = 0;
-		
-		timeB = System.currentTimeMillis();
-		SQLConnectionSingleton sqlconnection =  SQLConnectionSingleton.getInstance();
-		Connection connection = sqlconnection.getConnection();
-		timeA = System.currentTimeMillis();
-		System.out.println(timeA-timeB+'\n');
+		final SqlExecutor executor = timeExecutorCreation(() -> new SqlExecutor(SQLConnectionSingleton.getInstance()));
 		
 		try {
-		 Statement statement = connection.createStatement();
-	        String selectSql ="select * from users where id = 1";
-	        ResultSet resultSet = statement.executeQuery(selectSql);
+	        final ResultSet resultSet = executor.execute("select * from users where id = 1");
 	        while(resultSet.next()) {
-	        System.out.println("id: "+ resultSet.getInt("id"));
-	        System.out.println("name: "+ resultSet.getString("name"));
-	        System.out.println("email: "+ resultSet.getString("email"));
-	        System.out.println("university: "+ resultSet.getString("university"));
+	        	System.out.println("id: "+ resultSet.getInt("id"));
+	        	System.out.println("name: "+ resultSet.getString("name"));
+	        	System.out.println("email: "+ resultSet.getString("email"));
+	        	System.out.println("university: "+ resultSet.getString("university"));
 	        }
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
-		
-		timeB = System.currentTimeMillis();
-		SQLConnectionSingleton sqlconnection2 = SQLConnectionSingleton.getInstance();
-		Connection connection2 = sqlconnection.getConnection();
-		timeA = System.currentTimeMillis();
-		System.out.println(timeA-timeB+'\n');	
-		
+
+		timeExecutorCreation(() -> new SqlExecutor(SQLConnectionSingleton.getInstance()));
 		System.out.println("------------------------------\n");
 	}
 
@@ -80,20 +53,16 @@ public class Client {
 		
 		c1.createConnectionUsingNormalConnector();
 		c1.createConnectionUsingNormalConnector();
-		c1.createConnectionUsingNormalConnector();
-		c1.createConnectionUsingNormalConnector();
-
-		/*
-		Client c2 = new Client();
-		c2.createConnectionUsingSingletonConnector();
-		c2.createConnectionUsingSingletonConnector();
-		c2.createConnectionUsingSingletonConnector();
-		c2.createConnectionUsingSingletonConnector();
-		c2.createConnectionUsingSingletonConnector();
 		c1.createConnectionUsingSingletonConnector();
-		*/
+		c1.createConnectionUsingSingletonConnector();
+	}
 
-		
+	private SqlExecutor timeExecutorCreation(final Supplier<SqlExecutor> supplier){
+		final long startTime = System.currentTimeMillis();
+		final SqlExecutor sqlExecutor = supplier.get();
+		final long endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+		return sqlExecutor;
 	}
 }
 
