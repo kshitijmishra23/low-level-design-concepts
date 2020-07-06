c3b29be14007cb2f96298cfa027b3a968db608eb
Piyush Singh <piyush.singh@kronos.com>
2020-07-06 10:50:12 +0530

    Revert "srp"
    This reverts commit efcb84091c94ce832887f913f6d0f021b762c7c8.

--- src/oops/SOLID/singleResponsibilityPrinciple/piyush/model/FullTimeEmployee.java
+++
@@ -1,8 +0,0 @@
-package oops.SOLID.singleResponsibilityPrinciple.piyush.model;
-
-public class FullTimeEmployee extends Employee {
-	public FullTimeEmployee(String fullName, int monthlyIncome) {
-		super(fullName, monthlyIncome);
-		this.setNbHoursPerWeek(40);
-	}
-}
