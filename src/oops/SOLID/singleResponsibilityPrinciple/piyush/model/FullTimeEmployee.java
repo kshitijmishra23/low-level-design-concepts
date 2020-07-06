efcb84091c94ce832887f913f6d0f021b762c7c8
Piyush Singh <piyush.singh@kronos.com>
2020-07-06 10:16:28 +0530

    srp

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/piyush/model/FullTimeEmployee.java
@@ -0,0 +1,8 @@
+package oops.SOLID.singleResponsibilityPrinciple.piyush.model;
+
+public class FullTimeEmployee extends Employee {
+	public FullTimeEmployee(String fullName, int monthlyIncome) {
+		super(fullName, monthlyIncome);
+		this.setNbHoursPerWeek(40);
+	}
+}
