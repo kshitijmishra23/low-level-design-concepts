ff42f3fa78b2794f617a4a7f8889466d97b3fdf5
Piyush Singh <piyush.singh@kronos.com>
2020-07-09 13:29:47 +0530

    FORMATTING

--- src/oops/designPatterns/factory/websites/Website.java
+++ src/oops/designPatterns/factory/websites/Website.java
@@ -1,7 +1,5 @@
 package oops.designPatterns.factory.websites;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +7,15 @@ import oops.designPatterns.factory.pages.Page;
 
 public abstract class Website {
 
-	protected List<Page> pages = new ArrayList<>();
-	
-	public List<Page> getPages() {
-		return pages;
-	}
+  protected List<Page> pages = new ArrayList<>();
 
-	public Website() {
-		this.createWebsite();
-	}
-	
-	public abstract void createWebsite();
-	
+  public List<Page> getPages() {
+    return pages;
+  }
+
+  public Website() {
+    this.createWebsite();
+  }
+
+  public abstract void createWebsite();
 }
