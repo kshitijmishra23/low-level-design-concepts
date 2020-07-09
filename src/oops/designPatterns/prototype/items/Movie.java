76f4f971a8b0cd4ef340b1c20bfcd9c2b0f1430c
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-09 11:44:51 +0530

    Item classes

---
+++ src/oops/designPatterns/prototype/items/Movie.java
@@ -0,0 +1,16 @@
+package oops.designPatterns.prototype.items;
+
+
+public class Movie extends Item {
+
+	private String runtime;
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	
+}
