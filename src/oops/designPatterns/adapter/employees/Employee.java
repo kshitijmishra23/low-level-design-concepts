9654c06f138a2ba205a143a111113370ebe142fc
Piyush Singh <piyush.singh@kronos.com>
2020-07-10 21:54:47 +0530

    Added adaptors for CSV and LDAP type employee

--- src/oops/designPatterns/adapter/employees/Employee.java
+++ src/oops/designPatterns/adapter/employees/Employee.java
@@ -1,11 +1,12 @@
 package oops.designPatterns.adapter.employees;
 
-
 public interface Employee {
 
-	public String getId();
-	public String getFirstName();
-	public String getLastName();
-	public String getEmail();
+  public String getId();
 
+  public String getFirstName();
+
+  public String getLastName();
+
+  public String getEmail();
 }
