7c752df13c530c996fd2143efc5b12f659e7beb0
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-07-11 17:40:25 +0530

    adding content to textBox

--- src/oops/designPatterns/decorator/visualComponents/TextView.java
+++ src/oops/designPatterns/decorator/visualComponents/TextView.java
@@ -2,9 +2,23 @@ package oops.designPatterns.decorator.visualComponents;
 
 public class TextView extends VisualComponent{
 	
+	String content;
+	
+	public TextView(String content) {
+		this.content = content;
+	}
 	@Override
 	public void draw() {
-		System.out.println("Text View Drawn");
+		System.out.println("Text View Drawn with the content: " + content);
 	}
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 
 }
