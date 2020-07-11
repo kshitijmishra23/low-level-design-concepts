35d424acd88de5da730a0b30df70acddb293f63d
Rahul Sarkar <rahul.sarkar@toppr.com>
2020-07-11 17:17:21 +0530

    ocp changes

--- src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
+++ src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
@@ -18,18 +18,29 @@ public class CalculateTaxesClient {
         List<Employee> employees = repository.findAll();
 
         // Calculate taxes
-        Locale locale = new Locale("en", "US");
+        NumberFormat currencyFormatter = CalculateTaxesClient.getCurrencyFormatter("en", "US");
+       
+        double totalTaxes = CalculateTaxesClient.totalTax(employees, currencyFormatter);
+    }
+    
+    public static NumberFormat getCurrencyFormatter(String language, String country) {
+    	Locale locale = new Locale(language, country);
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
-        TaxCalculator taxCalculator = new TaxCalculator();
-
-        double totalTaxes = 0;
+        return currencyFormatter;
+    }
+    
+    public static double totalTax(List<Employee> employees, NumberFormat currencyFormatter) {
+    	double totalTaxes = 0;
+    	TaxCalculator taxCalculator = new TaxCalculator();
         for (Employee employee: employees){
-
+        	
             // compute individual tax
             double tax = taxCalculator.calculate(employee);
             String formattedTax = currencyFormatter.format(tax);
             // add to company total taxes
             totalTaxes += taxCalculator.calculate(employee);
         }
+        return totalTaxes;
     }
+    
 }
--- src/oops/SOLID/openClosePrinciple/before/taxes/TaxCalculator.java
+++ src/oops/SOLID/openClosePrinciple/before/taxes/TaxCalculator.java
@@ -1,16 +1,16 @@
-package oops.SOLID.openClosePrinciple.before.taxes;
-
-import oops.SOLID.openClosePrinciple.before.employees.Employee;
-
-public class TaxCalculator {
-    private final int INCOME_TAX_PERCENTAGE = 20;
-    private final int PROFESSIONAL_TAX_PERCENTAGE = 3;
-
-
-    public double calculate(Employee employee) {
-        return 
-                (employee.getMonthlyIncome() * PROFESSIONAL_TAX_PERCENTAGE) / 100 +
-                (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
-
-    }
-}
+//package oops.SOLID.openClosePrinciple.before.taxes;
+//
+//import oops.SOLID.openClosePrinciple.before.employees.Employee;
+//
+//public class TaxCalculator {
+//    private final int INCOME_TAX_PERCENTAGE = 20;
+//    private final int PROFESSIONAL_TAX_PERCENTAGE = 3;
+//
+//
+//    public double calculate(Employee employee) {
+//        return 
+//                (employee.getMonthlyIncome() * PROFESSIONAL_TAX_PERCENTAGE) / 100 +
+//                (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
+//
+//    }
+//}
