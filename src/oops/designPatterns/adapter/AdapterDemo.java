38e23ed0e40699b41667f49552a90ba8e370d0c1
amrojsandhu <amrojpals@gmail.com>
2020-07-13 18:22:40 +0530

    Scaler-200708: Day 7 assignments

--- src/oops/designPatterns/adapter/AdapterDemo.java
+++ src/oops/designPatterns/adapter/AdapterDemo.java
@@ -7,11 +7,14 @@ import oops.designPatterns.adapter.employees.Employee;
 
 public class AdapterDemo {
 
-	public static void main(String[] args) {
-		EmployeeClient client = new EmployeeClient();
-		
-		List<Employee> employees = client.getEmployeeList();
-		
-		System.out.println(employees);
-	}
+    public static void main(String[] args) {
+        EmployeeClient client = new EmployeeClient();
+
+        List<Employee> employees = client.getEmployeeList();
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+    }
 }
--- src/oops/designPatterns/adapter/EmployeeClient.java
+++ src/oops/designPatterns/adapter/EmployeeClient.java
@@ -13,22 +13,27 @@ import oops.designPatterns.adapter.employees.EmployeeLdap;
 
 public class EmployeeClient {
 
-	public List<Employee> getEmployeeList() {
-	
-		List<Employee> employees = new ArrayList<>();
-		
-		Employee employeeFromDB = new EmployeeDB("1234", "John", "Wick", "john@wick.com");
-		
-		EmployeeLdap employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");		
-		
-		EmployeeCSV employeeFromCSV = new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com");
-		
-		//We have to add the data of all the employees to the employees list
-		//But it only accepts objects of type Employee
-		
-		
-		return employees;
-		
-	}
-	
+    public List<Employee> getEmployeeList() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        Employee employeeFromDB = new EmployeeDB("1234", "John", "Wick", "john@wick.com");
+
+        EmployeeAdapterLdap employeeFromLdap = new EmployeeAdapterLdap(
+        		new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com"));
+
+        EmployeeAdapterCSV employeeFromCSV = new EmployeeAdapterCSV(
+        		new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com"));
+
+        //We have to add the data of all the employees to the employees list
+        //But it only accepts objects of type Employee
+
+		employees.add(employeeFromDB);
+		employees.add(employeeFromLdap);
+		employees.add(employeeFromCSV);
+
+        return employees;
+
+    }
+
 }
