9c728538cefabbe2cbcc82c304065ee9c6d2e0ab
ishanroy <ishanroy.nitald@gmail.com>
2020-07-15 06:02:31 +0530

    added client implementation

--- src/oops/designPatterns/decorator/Client.java
+++ src/oops/designPatterns/decorator/Client.java
@@ -1,10 +1,20 @@
 package oops.designPatterns.decorator;
 
+import oops.designPatterns.decorator.visualComponents.TextView;
+import oops.designPatterns.decorator.visualComponents.VisualComponent;
+import oops.designPatterns.decorator.visualComponents.decorators.BorderDecorator;
+import oops.designPatterns.decorator.visualComponents.decorators.ScrollDecorator;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+
 		//Draw a textBox with Border and Scroll feature here
+		VisualComponent textBox = new TextView("This is a text-box.");
+		textBox = new BorderDecorator(textBox);
+		textBox = new ScrollDecorator(textBox);
+
+		textBox.draw();
 
 	}
 
