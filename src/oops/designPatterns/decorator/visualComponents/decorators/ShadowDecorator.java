e0b8a00a315daf91d15859eab54147c78d00cff2
ishanroy <ishanroy.nitald@gmail.com>
2020-07-15 06:24:15 +0530

    added ButtonDecorator

--- src/oops/designPatterns/decorator/visualComponents/decorators/ShadowDecorator.java
+++ src/oops/designPatterns/decorator/visualComponents/decorators/ShadowDecorator.java
@@ -3,14 +3,15 @@ package oops.designPatterns.decorator.visualComponents.decorators;
 
 import oops.designPatterns.decorator.visualComponents.VisualComponent;
 
-public class ButtonDecorator extends VisualComponentDecorator {
+public class ShadowDecorator extends VisualComponentDecorator {
 
-    public ButtonDecorator(VisualComponent visualComponent) {
+    public ShadowDecorator(VisualComponent visualComponent) {
         super(visualComponent);
     }
 
     @Override
     public void draw() {
-        System.out.println("Added button.");
+        this.visualComponent.draw();
+        System.out.println("Added shadow.");
     }
 }
