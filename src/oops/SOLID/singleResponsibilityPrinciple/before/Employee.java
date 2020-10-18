62977949ce645cd516cc177f10aa9c7dbb1bc3f1
Tousif Ahmed <tousif.bits@gmail.com>
2020-10-18 17:00:15 +0530

    fixed SRP violation in Employee by creating write functionality in EmployeeWriter

--- src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
@@ -18,8 +18,8 @@ public abstract class Employee {
         setMonthlyIncome(monthlyIncome);
 
         String[] names = fullName.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[1];
+        this.firstName = names.length >= 1 ? names[0] : "";
+        this.lastName = names.length >= 2 ? names[1] : "";
     }
 
     public String getEmail() {
@@ -33,6 +33,14 @@ public abstract class Employee {
         return this.firstName + " " +
                 this.lastName + " - " +
                 this.monthlyIncome;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public int getMonthlyIncome() {
@@ -61,34 +69,5 @@ public abstract class Employee {
 
     public String getFullName(){
         return this.firstName + " " + this.lastName;
-    }
-
-    public  void save(){
-        try {
-        	Employee employee =this;
-            StringBuilder sb = new StringBuilder();
-            sb.append("### EMPLOYEE RECORD ####");
-            sb.append(System.lineSeparator());
-            sb.append("NAME: ");
-            sb.append(employee.firstName + " " + employee.lastName);
-            sb.append(System.lineSeparator());
-            sb.append("POSITION: ");
-            sb.append(employee.getClass().getTypeName());
-            sb.append(System.lineSeparator());
-            sb.append("EMAIL: ");
-            sb.append(employee.getEmail());
-            sb.append(System.lineSeparator());
-            sb.append("MONTHLY WAGE: ");
-            sb.append(employee.monthlyIncome);
-            sb.append(System.lineSeparator());
-
-            Path path = Paths.get(employee.getFullName()
-                    .replace(" ","_") + ".rec");
-            Files.write(path, sb.toString().getBytes());
-
-            System.out.println("Saved employee " + employee.toString());
-        } catch (IOException e){
-            System.out.println("ERROR: Could not save employee. " + e);
-        }
     }
 }
