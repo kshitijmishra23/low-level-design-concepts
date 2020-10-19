42a63173ce94373a42ca16f3fe27abe816f010eb
i-Shanky <deep@ASUS-ROG.localdomain>
2020-10-19 22:43:00 +0530

    SRP Assignment

---
+++ src/oops/SOLID/singleResponsibilityPrinciple/after/FullTimeEmployee.java
@@ -0,0 +1,8 @@
+package oops.SOLID.singleResponsibilityPrinciple.before;
+
+public class FullTimeEmployee extends Employee {
+    public FullTimeEmployee(String fullName, int monthlyIncome) {
+        super(fullName, monthlyIncome);
+        this.setNbHoursPerWeek(40);
+    }
+}
