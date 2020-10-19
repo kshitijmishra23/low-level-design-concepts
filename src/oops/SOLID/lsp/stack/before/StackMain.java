89481a1ccdd1d48e91ce357d681c1a7af618a955
Tousif Ahmed <tousif.bits@gmail.com>
2020-10-19 12:58:04 +0530

    Replaced StackCorrect implementation with AbstractStack: coding to interface or super class and not to implementation

--- src/oops/SOLID/lsp/stack/before/StackMain.java
+++ src/oops/SOLID/lsp/stack/before/StackMain.java
@@ -2,7 +2,7 @@ package oops.SOLID.lsp.stack.before;
 
 public class StackMain {
     public static void main(String[] args) {
-        StackCorrect<Integer> st = new StackCorrect<>();
+        AbstractStack<Integer> st = new StackCorrect<>();
         st.push(1);
         st.push(2);
         System.out.println(st.top());
