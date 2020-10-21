d3cfef3a070b652eeb75f6aff1471e5276be00c2
hareeshkj <hareesh.kj@gmail.com>
2020-10-22 01:31:19 +0530

    Update CalculateTaxesClient.java
    Modified to calculate tax from the taxutil classes which implement TaxCalcInterface

--- src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
+++ src/oops/SOLID/openClosePrinciple/before/client/CalculateTaxesClient.java
@@ -6,14 +6,15 @@ import java.util.Locale;
 
 import oops.SOLID.openClosePrinciple.before.employees.Employee;
 import oops.SOLID.openClosePrinciple.before.persistence.EmployeeRepository;
-import oops.SOLID.openClosePrinciple.before.taxes.TaxCalculator;
-
+//import oops.SOLID.openClosePrinciple.before.taxes.TaxCalculator;
+import oops.SOLID.openClosePrinciple.before.taxes.TaxCalcInterface;
+import oops.SOLID.openClosePrinciple.before.taxes.TaxCalcFactorty;
 
 public class CalculateTaxesClient {
     public static void main(String[] args) {
        
         EmployeeRepository repository = new EmployeeRepository();
-
+        TaxCalcInterface TaxCalculator;
         // Grab employees
         List<Employee> employees = repository.findAll();
 
@@ -24,12 +25,15 @@ public class CalculateTaxesClient {
 
         double totalTaxes = 0;
         for (Employee employee: employees){
-
+            TaxCalculator = TaxCalcFactorty.getTaxCalculator(employee);
+            double tax = TaxCalculator.calculateTax(employee.getMonthlyIncome());
+            
             // compute individual tax
-            double tax = TaxCalculator.calculate(employee);
+            //double tax = TaxCalculator.calculate(employee);
             String formattedTax = currencyFormatter.format(tax);
             // add to company total taxes
-            totalTaxes += TaxCalculator.calculate(employee);
+            //totalTaxes += TaxCalculator.calculate(employee);
+            totalTaxes += tax;
         }
     }
 }
