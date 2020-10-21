89a2a1d0cd6f3d161469d6b33b72195b6222b2c0
hareeshkj <hareesh.kj@gmail.com>
2020-10-22 01:21:39 +0530

    Update TaxCalcUtilFactory.java

--- src/oops/SOLID/openClosePrinciple/before/taxes/TaxCalcUtilFactory.java
+++ src/oops/SOLID/openClosePrinciple/before/taxes/TaxCalcUtilFactory.java
@@ -1,6 +1,14 @@
 
 class TaxCalcUtilFactory{
     public TaxCalcInterface getTaxCalculator(Employee e){
-        
+         if (e.getNbHoursPerWeek() == 40){
+             return new TaxCalcUtilFTE();
+         }
+         else if (e.getNbHoursPerWeek() == 20){
+             return new TaxCalcUtilPTE();
+         }
+         else{
+             return new TaxCalcUtilINT();
+         }
     }
 }
