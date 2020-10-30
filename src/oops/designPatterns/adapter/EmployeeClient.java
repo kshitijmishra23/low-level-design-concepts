b0e57aa91d1c478435c952b4303d23e7edee1709
hareeshkj <hareesh.kj@gmail.com>
2020-10-31 01:57:24 +0530

    Update EmployeeClient.java

--- src/oops/designPatterns/adapter/EmployeeClient.java
+++ src/oops/designPatterns/adapter/EmployeeClient.java
@@ -8,6 +8,8 @@ import oops.designPatterns.adapter.employees.Employee;
 import oops.designPatterns.adapter.employees.EmployeeCSV;
 import oops.designPatterns.adapter.employees.EmployeeDB;
 import oops.designPatterns.adapter.employees.EmployeeLdap;
+import oops.designPatterns.adapter.employees.EmployeeCSVAdapter;
+import oops.designPatterns.adapter.employees.EmployeeLdapAdapter;
 
 public class EmployeeClient {
 
@@ -17,13 +19,17 @@ public class EmployeeClient {
 		
 		Employee employeeFromDB = new EmployeeDB("1234", "John", "Wick", "john@wick.com");
 		
-		EmployeeLdap employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");		
+		//EmployeeLdap employeeFromLdap = new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com");		
+		Employee employeeFromLdap = new EmployeeLdapAdapter(new EmployeeLdap("chewie", "Solo", "Han", "han@solo.com"));
 		
-		EmployeeCSV employeeFromCSV = new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com");
-		
+		//EmployeeCSV employeeFromCSV = new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com");
+		Employee employeeFromCSV = new EmployeeCSVAdapter(new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com"));
 		//We have to add the data of all the employees to the employees list
 		//But it only accepts objects of type Employee
 		
+		employees.add(employeeFromDB);
+		employees.add(employeeFromLdap);
+		employees.add(employeeFromCSV);
 		
 		return employees;
 		
