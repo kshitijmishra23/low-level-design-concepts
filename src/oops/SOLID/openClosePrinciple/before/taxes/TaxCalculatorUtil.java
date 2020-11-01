a310e84d7a154aad37f17b1ca04205991cbb0c56
Your Name <hsinghania000@gmail.com>
2020-11-01 20:21:27 +0530

    Changes made according to new requirements

---
+++ src/oops/SOLID/openClosePrinciple/before/taxes/TaxCalculatorUtil.java
@@ -0,0 +1,8 @@
+package oops.SOLID.openClosePrinciple.before.taxes;
+
+import oops.SOLID.openClosePrinciple.before.employees.Employee;
+
+public interface TaxCalculatorUtil {
+	
+    public double calculate(Employee employee);
+}
