45837f5927f5252de8befbf1ca741f9ee8d92672
Jayesh Khandagle <jayeshkhandagle123@gmail.com>
2020-12-07 01:48:29 +0530

    Added Changes to fix LSP issue.

---
+++ src/oops/SOLID/lsp/stack/before/Stacks.java
@@ -0,0 +1,14 @@
+package oops.SOLID.lsp.stack.before;
+
+public interface Stacks {
+
+    public void push(Integer a);
+
+    public void pop();
+
+    public Integer top();
+
+    public void clear();
+
+
+}
