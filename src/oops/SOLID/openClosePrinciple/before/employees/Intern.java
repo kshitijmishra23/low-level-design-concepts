9583ce7e8256102802338704f9cdc6d21f5ffb87
kushb2 <kushkkr08@gmail.com>
2021-04-08 12:29:22 +0530

    Batch :  ACADEMY OCT-NOV 20 SUPERX , LLD1 , email : bhardwaj.kusho2@gmail.com
    Batch :  ACADEMY OCT-NOV 20 SUPERX , LLD1 , email : bhardwaj.kusho2@gmail.com
    HW:
    
    1. Refactor this code such that it does not violate the SRP principle.
    2. Refactor this code such that it does not violate the LSP principle.
    3. You are given the implementation for this business requirement here. Change it for the new requirements.

--- src/oops/SOLID/openClosePrinciple/before/employees/Intern.java
+++ src/oops/SOLID/openClosePrinciple/before/employees/Intern.java
@@ -1,8 +1,27 @@
 package oops.SOLID.openClosePrinciple.before.employees;
 
-public class Intern extends Employee {
-    public Intern(String fullName, int monthlyIncome, int nbHours) {
+public class Intern extends Employee implements IncomeTax{
+   
+	private final static int INCOME_TAX_PERCENTAGE = 15;
+	
+	public Intern(String fullName, int monthlyIncome, int nbHours) {
         super(fullName, monthlyIncome);
         setNbHoursPerWeek(nbHours);
     }
+
+	@Override
+	public double calculate() {
+		Double tax = 0.0;
+	
+		if ((this.getMonthlyIncome() * 12) > 30000) {
+			tax = (getIncomeTax() * getIncomeTax()) / 100.0;
+		}
+		return tax;
+	}
+
+	
+	@Override
+	public int getIncomeTax() {
+		return INCOME_TAX_PERCENTAGE;
+	}
 }
