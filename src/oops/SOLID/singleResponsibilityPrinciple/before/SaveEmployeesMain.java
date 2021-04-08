fe3a21e8a56aeda24e320f1e288d0a40fa3a1ea9
vinit.kayarkar <vkayarkar@deloitte.com>
2021-04-09 01:19:58 +0530

    Changes for SRP and LS principles

--- src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
@@ -5,12 +5,15 @@ import java.util.List;
 public class SaveEmployeesMain {
     public static void main(String[] args) {
         // Grab employees
+        List<Employee> empLi = List.of(new FullTimeEmployee("Anna Smith", 2000),
+                                       new FullTimeEmployee("Billy Leech", 920),
+                                       new PartTimeEmployee("Steve Jones", 800),
+                                       new PartTimeEmployee("Magda Iovan", 920));
+
         EmployeeRepository repository = new EmployeeRepository();
         List<Employee> employees = repository.findAll();
 
         // Save all
-        for (Employee e : employees){
-            e.save();
-        }
+        repository.saveAll(empLi);
     }
 }
