e874b8f423d52afb024887bfa40c554560811924
aakuyr <issacnewton.sai@gmail.com>
2021-04-12 15:22:39 +0530

    Made a small change in client

--- src/oops/SOLID/openClosePrinciple/before/taxes/InternTaxCalculator.java
+++ src/oops/SOLID/openClosePrinciple/before/taxes/InternTaxCalculator.java
@@ -2,15 +2,14 @@ package oops.SOLID.openClosePrinciple.before.taxes;
 
 import oops.SOLID.openClosePrinciple.before.employees.Intern;
 
-public class InternTaxCalculator implements TaxCalculator<Intern>{
-
+public class InternTaxCalculator implements TaxCalculator<Intern> {
     @Override
     public double calculate(Intern employee) {
         Integer income = employee.getMonthlyIncome();
         if (income < 300000) {
             return 0;
         } else {
-            return income * 15/100;
+            return income * 15 / 100;
         }
     }
 }
--- src/oops/SOLID/openClosePrinciple/before/taxes/PartTimeEmployeeTaxCalculator.java
+++ src/oops/SOLID/openClosePrinciple/before/taxes/PartTimeEmployeeTaxCalculator.java
@@ -3,7 +3,7 @@ package oops.SOLID.openClosePrinciple.before.taxes;
 import oops.SOLID.openClosePrinciple.before.employees.Employee;
 import oops.SOLID.openClosePrinciple.before.employees.PartTimeEmployee;
 
-public class PartTimeEmployeeTaxCalculator implements TaxCalculator<PartTimeEmployee>  {
+public class PartTimeEmployeeTaxCalculator implements TaxCalculator<PartTimeEmployee> {
     private static final Integer INCOME_TAX = 20;
     private static final Integer PROFESSIONAL_TAX = 3;
     private static final Integer EDUCATION_CESS = 1;
