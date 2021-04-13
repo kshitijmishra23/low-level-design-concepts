9c1e711dfbadc81d6b4fe073a9cc9c351a0cf692
Akshay Thakur <Akshay.Thakur@ibm.com>
2021-04-13 13:28:04 +0530

    Update SaveEmployeesMain.java

--- src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
@@ -1,6 +1,11 @@
 package oops.SOLID.singleResponsibilityPrinciple.before;
 
 import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class SaveEmployeesMain {
     public static void main(String[] args) {
@@ -10,7 +15,36 @@ public class SaveEmployeesMain {
 
         // Save all
         for (Employee e : employees){
-            e.save();
+            repository.save(e);
+        }
+    }
+
+     public static void save(Employee employee){
+        try {
+        	Employee employee =this;
+            StringBuilder sb = new StringBuilder();
+            sb.append("### EMPLOYEE RECORD ####");
+            sb.append(System.lineSeparator());
+            sb.append("NAME: ");
+            sb.append(employee.firstName + " " + employee.lastName);
+            sb.append(System.lineSeparator());
+            sb.append("POSITION: ");
+            sb.append(employee.getClass().getTypeName());
+            sb.append(System.lineSeparator());
+            sb.append("EMAIL: ");
+            sb.append(employee.getEmail());
+            sb.append(System.lineSeparator());
+            sb.append("MONTHLY WAGE: ");
+            sb.append(employee.monthlyIncome);
+            sb.append(System.lineSeparator());
+
+            Path path = Paths.get(employee.getFullName()
+                    .replace(" ","_") + ".rec");
+            Files.write(path, sb.toString().getBytes());
+
+            System.out.println("Saved employee " + employee.toString());
+        } catch (IOException e){
+            System.out.println("ERROR: Could not save employee. " + e);
         }
     }
 }
