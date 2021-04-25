a9e803a4d38e57a09b9c2a39efe5ede4694fcfe2
Aman Anand <Aman.Anand@sumtotalsystems.com>
2021-04-25 17:20:53 +0530

    fixing single responsiblity principle violation

--- src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/SaveEmployeesMain.java
@@ -8,9 +8,20 @@ public class SaveEmployeesMain {
         EmployeeRepository repository = new EmployeeRepository();
         List<Employee> employees = repository.findAll();
 
+        public  void save(ISavable savable){
+            try {
+                Path path = Paths.get(savable.getFileName());
+                Files.write(path, savable.getSavableData().getBytes());
+
+                System.out.println("Saved employee " + employee.toString());
+            } catch (IOException e){
+                System.out.println("ERROR: Could not save employee. " + e);
+            }
+        }
+
         // Save all
         for (Employee e : employees){
-            e.save();
+            save(e);
         }
     }
 }
