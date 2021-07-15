14186c62e4cc8834c4edeefd4c771b6944af1465
ankur8969 <ankur.kr810@gmail.com>
2021-07-15 13:37:49 +0530

    improved SRP implementation

--- src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
@@ -1,10 +1,5 @@
 package oops.SOLID.singleResponsibilityPrinciple.before;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /*
 Models an employee form a business perspective
  */
@@ -62,42 +57,5 @@ public abstract class Employee {
     public String getFullName(){
         return this.firstName + " " + this.lastName;
     }
-
-    public String getEmpDataToStore(){
-        StringBuilder sb = new StringBuilder();
-            sb.append("### EMPLOYEE RECORD ####");
-            sb.append(System.lineSeparator());
-            sb.append("NAME: ");
-            sb.append(employee.firstName + " " + employee.lastName);
-            sb.append(System.lineSeparator());
-            sb.append("POSITION: ");
-            sb.append(employee.getClass().getTypeName());
-            sb.append(System.lineSeparator());
-            sb.append("EMAIL: ");
-            sb.append(employee.getEmail());
-            sb.append(System.lineSeparator());
-            sb.append("MONTHLY WAGE: ");
-            sb.append(employee.monthlyIncome);
-            sb.append(System.lineSeparator());
-            return sb.toString();
-    }
-
-    public String getPath(){
-        Employee employee =this;
-        return Paths.get(employee.getFullName()
-                    .replace(" ","_") + ".rec");
-    }
-
-    public  void save(){
-        try {
-        	Employee employee =this;
-            Path path = employee.getPath();
-            String sb=employee.getEmpDataToStore();
-            Files.write(path, sb.getBytes());
-
-            System.out.println("Saved employee " + employee.toString());
-        } catch (IOException e){
-            System.out.println("ERROR: Could not save employee. " + e);
-        }
-    }
+ 
 }
