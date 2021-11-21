7b272ef0b920aca4e962c3e385308cf1faf7be5f
sagar <yadavsagar783@gmail.com>
2021-11-21 15:33:33 +0530

    srp

--- src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
@@ -2,6 +2,7 @@ package oops.SOLID.singleResponsibilityPrinciple.before;
 
 import java.util.Arrays;
 import java.util.List;
+import java.nio.file.Path;
 
 public class EmployeeRepository {
 
@@ -18,12 +19,6 @@ public class EmployeeRepository {
     }
 
     public void saveEmployee(Path path,StringBuilder sb){
-        try {
-            Files.write(path, sb.toString().getBytes());
-            System.out.println("Saved employee " + employee.toString());
-        } catch (IOException e){
-            System.out.println("ERROR: Could not save employee. " + e);
-        }
-
+            filedb.save(path, sb.toString().getBytes())
     }
 }
